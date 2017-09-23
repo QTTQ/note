a8067a2ef6c7e8a826a3c46431ca5821ec79db49
unknown <Administrator@JHLC03ZJ0039.ad5173.com>
2017-09-23 20:40:02 +0800

    2017-9-23

--- 2017-9-23/props子组件向父组件通信和子组件自己定制不同模板.js
+++ 2017-9-23/props子组件向父组件通信和子组件自己定制不同模板.js
@@ -1,10 +1,10 @@
 // 父组件可以在使用子组件的地方直接用 v-on 来监听子组件触发的事件。
 
-{/* <div id="counter-event-example">
-  <p>{{ total }}</p>
-  <button-counter v-on:increment="incrementTotal"></button-counter>
-  <button-counter v-on:increment="incrementTotal"></button-counter>
-</div> */}
+// /* <div id="counter-event-example">
+//   <p>{{ total }}</p>
+//   <button-counter v-on:increment="incrementTotal"></button-counter>
+//   <button-counter v-on:increment="incrementTotal"></button-counter>
+// </div> */
 
 Vue.component('button-counter', {
   template: '<button v-on:click="incrementCounter">{{ counter }}</button>',
@@ -32,14 +32,12 @@ new Vue({
   }
 }
 
-{/* 定制一样模板 不同显示数据 */}
+// /* 定制一样模板 不同显示数据 */
 
-    <div id="app">
-        <c-h bt-n="hhahah"></c-h>
-        <c-h bt-n="sdasdas"></c-h>
-    </div>
-    <script src="./vue.js"></script>
-    <script>
+    // <div id="app">
+    //     <c-h bt-n="hhahah"></c-h>
+    //     <c-h bt-n="sdasdas"></c-h>
+    // </div>
         let cH = {
             props: ['btN'],
             template: `<div>
@@ -58,4 +56,3 @@ new Vue({
                 cH
             }
         })
-    </script>
