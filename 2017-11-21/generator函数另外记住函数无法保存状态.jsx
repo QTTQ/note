c022fa2814510b69284142133a3a095fbaa087af
Avrin <1321510155@qq.com>
2017-11-22 17:01:23 +0800

    generator函数另外记住函数无法保存状态

---
+++ 2017-11-21/generator函数另外记住函数无法保存状态.jsx
@@ -0,0 +1,51 @@
+函数无法保存状态
+
+
+要编写一个产生斐波那契数列的函数，可以这么写：
+
+function fib(max) {
+    var
+        t,
+        a = 0,
+        b = 1,
+        arr = [0, 1];
+    while (arr.length < max) {
+        [a, b] = [b, a + b];
+        arr.push(b);
+    }
+    return arr;
+}
+
+// 测试:
+fib(5); // [0, 1, 1, 2, 3]
+fib(10); // [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+函数只能返回一次，所以必须返回一个Array。但是，如果换成generator，
+就可以一次返回一个数，不断返回多次。用generator改写如下：
+
+function* fib(max) {
+    var
+        t,
+        a = 0,
+        b = 1,
+        n = 0;
+    while (n < max) {
+        yield a;
+        [a, b] = [b, a + b];
+        n ++;
+    }
+    return;
+}
+直接调用试试：
+
+fib(5); // fib {[[GeneratorStatus]]: "suspended", [[GeneratorReceiver]]: Window}
+直接调用一个generator和调用函数不一样，fib(5)仅仅是创建了一个generator对象，还没有去执行它。
+
+调用generator对象有两个方法，一是不断地调用generator对象的next()方法：
+
+var f = fib(5);
+f.next(); // {value: 0, done: false}
+f.next(); // {value: 1, done: false}
+f.next(); // {value: 1, done: false}
+f.next(); // {value: 2, done: false}
+f.next(); // {value: 3, done: false}
+f.next(); // {value: undefined, done: true}
