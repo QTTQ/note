664e5cd81d6776008e1c13d90fd672010062f124
QTTQ <1321510155@qq.com>
2018-07-09 09:05:38 +0800

    git 合并主分支

--- 3go/base.go
+++ 3go/base.go
@@ -1,44 +1,26 @@
 package main
-
 import (
 	"fmt"
-	"math"
+	"reflect"
 )
+type TagType struct{
+	fd1 bool "aaaaa"
+	fd2 string "bbbb"
+	fd3 int "cccccc"
+}
 
 
-var PI=3.14
-var name = "gopher"
 
-type newType int
+func main (){
+	tt:=TagType{true,"Barak Obama",1}
+	for i :=0;i<3;i++{
+		refTag(tt,i)
+	}
+}
+func refTag(tt TagType,ix int){
+	ttType:=reflect.TypeOf(tt)
+	ixField:=ttType.Field(ix)
+	// fmt.Printf("%v\n", ixField.Tag)
+	fmt.Println(ixField.Tag)
 
-type gopher struct{}
-
-type golang interface{}
-
-type byte int8
-type rune int32
-
-func main(){
-	var a[1]bool
-	var b 文本
-	b="哈哈哈哈"
-	var c int =1
-	d:=false
-	var x,y,z=1,2,3
-	xx,_,zz:=3,2,1
-	var aa float32=100.1
-	bb:=int(aa)
-	fmt.Println(bb)
-	fmt.Println("Hello Go!")
-	fmt.Println(a)
-	fmt.Println(math.MinInt8)
-	fmt.Println(math.MaxInt8)
-	fmt.Println(b)
-	fmt.Println(c)
-	fmt.Println(d)
-	fmt.Println(x)
-	fmt.Println(y)
-	fmt.Println(z)
-	fmt.Println(xx)
-	fmt.Println(zz)
 }
---
+++ git合并主分支.md
@@ -0,0 +1,10 @@
+git branch 查看分支
+git checkout -b QTTQ 新建分支
+
+git checkout master 切换到主分支
+
+git pull 拉取主分支内容
+
+git checkout QTTQ 切换到分支
+
+git pull origin master 合并主分支
