a2acfbac38f9f6ac3fb406901259905b1b5ed8e1
unknown <Administrator@get_pc_01.info.work.cn>
2019-06-25 18:15:39 +0800

    commit

--- 经典算法/problem009/problem009.go
+++ 经典算法/problem009/problem009.go
@@ -1,16 +1,22 @@
-package problem009
+package main
 
-func Fibonacci(n int)int  {
-	if n<=1{
+import (
+	"fmt"
+)
+
+func Fibonacci(n int) int {
+	if n <= 1 {
 		return n
 	}
-f1,f2:=0,1
-res:=0
-for i:=2;i<=n;i++{
-	if i>=2{
-		res=f1+f2
-	f1,f2=f2,res
+	f1, f2 := 0, 1
+	for i := 2; i <= n; i++ {
+		if i >= 2 {
+			f1, f2 = f2, f1+f2
+		}
 	}
+	return f1 + f2
 }
-return res
+
+func main() {
+	fmt.Println(Fibonacci(10))
 }
