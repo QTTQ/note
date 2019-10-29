e0a39a63553359d4c2ae7a61d17154201aadd344
arvin <1321510155@qq.com>
2019-10-29 16:39:37 +0800

    commit

--- docker/aa.jsx
+++ docker/aa.jsx
@@ -224,6 +224,8 @@
 // 创建并运行容器
 // docker run -d -it --net=host --name amdl amdlserver:0.01
 
+// docker 常用命令
+// https://www.jianshu.com/p/afb20541d781
 
 // 日志 
 // docker log id
@@ -237,10 +239,63 @@
 // docker ps|grep redis
 
 
+// docker stop $(docker ps -a -q)  
+//首先停掉所有的容器
+
+// docker rm $(docker ps -aq) 
+//然后删除所有的容器
+
 
 // 介绍的文章 很不错
 https://www.cnblogs.com/lienhua34/p/5170335.html
 
+
+
+
+
+
+
+
+// docker run ：创建一个新的容器并运行一个命令
+ 
+// 语法
+// docker run [OPTIONS] IMAGE [COMMAND] [ARG...]
+// OPTIONS说明：
+ 
+// -a stdin: 指定标准输入输出内容类型，可选 STDIN/STDOUT/STDERR 三项；
+ 
+// -d: 后台运行容器，并返回容器ID；
+ 
+// -i: 以交互模式运行容器，通常与 -t 同时使用；
+ 
+// -p: 端口映射，格式为：主机(宿主)端口:容器端口
+ 
+// -t: 为容器重新分配一个伪输入终端，通常与 -i 同时使用；
+ 
+// --name="nginx-lb": 为容器指定一个名称；
+ 
+// --dns 8.8.8.8: 指定容器使用的DNS服务器，默认和宿主一致；
+ 
+// --dns-search example.com: 指定容器DNS搜索域名，默认和宿主一致；
+ 
+// -h "mars": 指定容器的hostname；
+ 
+// -e username="ritchie": 设置环境变量；
+ 
+// --env-file=[]: 从指定文件读入环境变量；
+ 
+// --cpuset="0-2" or --cpuset="0,1,2": 绑定容器到指定CPU运行；
+ 
+// -m :设置容器使用内存最大值；
+ 
+// --net="bridge": 指定容器的网络连接类型，支持 bridge/host/none/container: 四种类型；
+ 
+// --link=[]: 添加链接到另一个容器；
+ 
+// --expose=[]: 开放一个端口或一组端口；
+// ————————————————
+// 版权声明：本文为CSDN博主「giserinchina」的原创文章，遵循 CC 4.0 BY-SA 版权协议，转载请附上原文出处链接及本声明。
+// 原文链接：https://blog.csdn.net/wxb880114/article/details/82790323
 
 
 
